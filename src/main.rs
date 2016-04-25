62bbd93ceeb0fd4179695a1d361022eee081a325
Olle Sandberg <olle@b3rg.se>
2016-04-25 16:03:42 +0200

    Some clean up. Move drawing out of playfield object.

--- src/main.rs
+++ src/main.rs
@@ -7,34 +7,29 @@ use sdl2::render::Renderer;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use rand::Rng;
 
 
 struct Figure {
     blocks: [[[u8; 4]; 4]; 4]
 }
 
+/*
 struct RSTrisLiveFigure {
     fig: i32,
     x: i32,
     y: i32,
     dir: i32,
-}
+}*/
 
 struct RSTrisPlayfield {
-    pf_width: u32,
-    pf_height: u32,
+    pf_width: usize,
+    pf_height: usize,
     playfield: Vec<Vec<u8>>,
 }
 
 struct RSTris {
     figures: Vec<Figure>,
     pf: RSTrisPlayfield,
-/*
-    pf_width: u32,
-    pf_height: u32,
-    playfield: Vec<Vec<u8>>,
-*/
 //    current_figure: RSTrisLiveFigure,
     current_figure: i32,
     fig_dir: i32,
@@ -43,59 +38,57 @@ struct RSTris {
 }
 
 impl RSTrisPlayfield {
-    pub fn new(width: u32, height: u32) -> RSTrisPlayfield {
+    pub fn new(width: usize, height: usize) -> RSTrisPlayfield {
         let mut pf = RSTrisPlayfield{pf_width: width,
                                      pf_height: height,
                                      playfield: vec![]};
-        for h in 0..height {
+        for _ in 0..height {
             pf.playfield.push(vec![0; width as usize]);
         }
         pf
     }
-
-    fn draw_block(renderer: &mut sdl2::render::Renderer,
-                  x: i32, y: i32, color: Color) {
-        let block_width: i32 = 32;
-        let block_height: i32 = 32;
-        let block_spacing: i32 = 2;
-        renderer.set_draw_color(color);
-        let border_rect = Rect::new(x * block_width + x * block_spacing,
-                                    y * block_height + y * block_spacing,
-                                    block_width as u32, block_height as u32);
-        let _ = renderer.fill_rect(border_rect);
+    pub fn get_block(&self, x: usize, y: usize) -> u8 {
+        self.playfield[y][x]
+    }
+    pub fn width(&self) -> usize {
+        self.pf_width
+    }
+    pub fn height(&self) -> usize {
+        self.pf_height
     }
 
-    fn draw(&self, renderer: &mut sdl2::render::Renderer) {
-        let mut y: i32 = 0;
-        let frame_color = Color::RGB(200, 64, 64);
-        for row in &self.playfield {
-            let mut x: i32 = 0;
-            RSTrisPlayfield::draw_block(renderer, x, y, frame_color);
-            for col in row {
-                if *col != 0 {
-                    RSTrisPlayfield::draw_block(renderer, x + 1, y,
-                                                Color::RGB(0, 128, 128));
+    fn place_figure(&mut self, fig: &Figure, dir: i32, x: i32, y: i32) {
+        for row in 0..4 {
+            for col in 0..4 {
+                let b = fig.blocks[dir as usize][row][col];
+                if b != 0 {
+                    self.playfield[y as usize + row][x as usize + col] = b;
                 }
-                x += 1;
             }
-            RSTrisPlayfield::draw_block(renderer, x + 1, y, frame_color);
-            y += 1;
         }
-        for bottom in 0..(self.pf_width + 2) {
-            RSTrisPlayfield::draw_block(renderer, bottom as i32, y, frame_color);
+    }
+
+    fn rm_figure(&mut self, fig: &Figure, dir: i32, x: i32, y: i32) {
+        for row in 0..4 {
+            for col in 0..4 {
+                let b = fig.blocks[dir as usize][row][col];
+                if b != 0 {
+                    self.playfield[y as usize + row][x as usize + col] = 0;
+                }
+            }
         }
     }
 }
 
 impl RSTris {
 
-    pub fn new(width: u32, height: u32) -> RSTris {
-        let mut tris = RSTris{figures: vec![],
-                              pf: RSTrisPlayfield::new(width, height),
-                              current_figure: -1,
-                              fig_dir: 0,
-                              fig_x_pos: 0,
-                              fig_y_pos: 0};
+    pub fn new(width: usize, height: usize) -> RSTris {
+        let tris = RSTris{figures: vec![],
+                          pf: RSTrisPlayfield::new(width, height),
+                          current_figure: -1,
+                          fig_dir: 0,
+                          fig_x_pos: 0,
+                          fig_y_pos: 0};
         tris
     }
 
@@ -103,23 +96,22 @@ impl RSTris {
         self.figures.push(fig);
     }
 
-    fn place_figure(&mut self, fig: &Figure, dir: i32, x: i32, y: i32) {
-
-    }
-
-    fn rm_figure(&mut self, fig: &Figure, dir: i32, x: i32, y: i32) {
-
-    }
-
     fn update(&mut self) {
         if self.current_figure == -1 {
             self.current_figure = (rand::random::<u8>() % 7) as i32;
             self.fig_dir = 0;
-//            self.fig_x_pos = (self.pf_width / 2) as i32;
-//            self.fig_y_pos = 0;
+            self.fig_x_pos = (self.pf.pf_width / 2 - 1) as i32;
+            self.fig_y_pos = 0;
+            self.pf.place_figure(&self.figures[self.current_figure as usize],
+                                 self.fig_dir, self.fig_x_pos, self.fig_y_pos);
         }
         else {
             /* Move figure down one step */
+            self.pf.rm_figure(&self.figures[self.current_figure as usize],
+                              self.fig_dir, self.fig_x_pos, self.fig_y_pos);
+            self.fig_y_pos += 1;
+            self.pf.place_figure(&self.figures[self.current_figure as usize],
+                                 self.fig_dir, self.fig_x_pos, self.fig_y_pos);
         }
     }
 
@@ -128,7 +120,8 @@ impl RSTris {
             println!("Direction {}:", d);
             for y in 0..4 {
                 for x in 0..4 {
-                    print!("{}", if figure.blocks[d][y][x] == 0 {" " } else {"X"});
+                    print!("{}",
+                           if figure.blocks[d][y][x] == 0 {" " } else {"X"});
                 }
                 println!("");
             }
@@ -140,6 +133,37 @@ impl RSTris {
             RSTris::dump_figure(fig);
         }
 
+    }
+}
+
+fn draw_block(renderer: &mut Renderer, x: i32, y: i32, color: Color) {
+    let block_width: i32 = 32;
+    let block_height: i32 = 32;
+    let block_spacing: i32 = 2;
+    renderer.set_draw_color(color);
+    let border_rect = Rect::new(x * block_width + x * block_spacing,
+                                y * block_height + y * block_spacing,
+                                block_width as u32, block_height as u32);
+    let _ = renderer.fill_rect(border_rect);
+}
+
+fn draw_playfield(playfield: &RSTrisPlayfield, renderer: &mut Renderer) {
+    let frame_color = Color::RGB(200, 64, 64);
+    for y in 0..playfield.height() {
+        draw_block(renderer, 0, y as i32, frame_color);
+        for x in 0..playfield.width() {
+            let block = playfield.get_block(x, y);
+            if block != 0 {
+                draw_block(renderer, (x + 1) as i32, y as i32,
+                           Color::RGB(0, 128, 128));
+            }
+        }
+        draw_block(renderer, (playfield.width() + 1) as i32,
+                   y as i32, frame_color);
+    }
+    for bottom in 0..(playfield.pf_width + 2) {
+        draw_block(renderer, bottom as i32,
+                   playfield.height() as i32, frame_color);
     }
 }
 
@@ -301,7 +325,8 @@ fn main() {
         let current_ticks = time::precise_time_ns();
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                Event::Quit {..} | Event::KeyDown {
+                    keycode: Some(Keycode::Escape), .. } => {
                     break 'running
                 },
                 _ => {}
@@ -314,9 +339,9 @@ fn main() {
         }
 
         /* Render graphics */
-        let _ = renderer.set_draw_color(sdl2::pixels::Color::RGB(101, 208, 246));
+        let _ = renderer.set_draw_color(Color::RGB(101, 208, 246));
         let _ = renderer.clear();
-        rstris.pf.draw(&mut renderer);
+        draw_playfield(&rstris.pf, &mut renderer);
         let _ = renderer.present();
     }
 }
