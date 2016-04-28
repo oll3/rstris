9531c100851d1ada8e3534d6f5d2b6ccf60fdb3f
Olle Sandberg <olle@b3rg.se>
2016-04-28 16:16:25 +0200

    Split source files.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate sdl2;
-extern crate rand;
 extern crate time;
+
+mod rstris;
 
 use sdl2::rect::Rect;
 use sdl2::render::Renderer;
@@ -9,180 +10,6 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
 
-struct Figure {
-    blocks: [[[u8; 4]; 4]; 4]
-}
-
-/*
-struct RSTrisLiveFigure {
-    fig: i32,
-    x: i32,
-    y: i32,
-    dir: i32,
-}*/
-
-struct RSTrisPlayfield {
-    pf_width: usize,
-    pf_height: usize,
-    playfield: Vec<Vec<u8>>,
-}
-
-struct RSTris {
-    figures: Vec<Figure>,
-    pf: RSTrisPlayfield,
-//    current_figure: RSTrisLiveFigure,
-    current_figure: i32,
-    fig_dir: i32,
-    fig_x_pos: i32,
-    fig_y_pos: i32,
-}
-
-impl RSTrisPlayfield {
-    pub fn new(width: usize, height: usize) -> RSTrisPlayfield {
-        let mut pf = RSTrisPlayfield{pf_width: width,
-                                     pf_height: height,
-                                     playfield: vec![]};
-        for _ in 0..height {
-            pf.playfield.push(vec![0; width as usize]);
-        }
-        pf
-    }
-    pub fn get_block(&self, x: usize, y: usize) -> u8 {
-        self.playfield[y][x]
-    }
-    pub fn width(&self) -> usize {
-        self.pf_width
-    }
-    pub fn height(&self) -> usize {
-        self.pf_height
-    }
-
-    fn place_figure(&mut self, fig: &Figure, dir: i32, x: i32, y: i32) {
-        for row in 0..4 {
-            for col in 0..4 {
-                let b = fig.blocks[dir as usize][row][col];
-                if b != 0 {
-                    self.playfield[y as usize + row][x as usize + col] = b;
-                }
-            }
-        }
-    }
-
-    fn rm_figure(&mut self, fig: &Figure, dir: i32, x: i32, y: i32) {
-        for row in 0..4 {
-            for col in 0..4 {
-                let b = fig.blocks[dir as usize][row][col];
-                if b != 0 {
-                    self.playfield[y as usize + row][x as usize + col] = 0;
-                }
-            }
-        }
-    }
-
-    fn test_figure(&self, fig: &Figure, dir: i32, x: i32, y: i32) -> bool {
-        for row in 0..4 {
-            let offs_y  = y + row;
-            for col in 0..4 {
-                let offs_x  = x + col;
-                let b = fig.blocks[dir as usize][row as usize][col as usize];
-                if b != 0 {
-                    if offs_y < 0 || offs_y >= self.pf_height as i32 {
-                        return false;
-                    }
-                    if offs_x < 0 || offs_x >= self.pf_width as i32 {
-                        return false;
-                    }
-                    if self.playfield[offs_y as usize][offs_x as usize] != 0 {
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
-    }
-
-    fn move_figure(&mut self, fig: &Figure, dir: i32, x: i32, y: i32,
-                   x_offs: i32, y_offs: i32) -> bool {
-        self.rm_figure(fig, dir, x, y);
-        if self.test_figure(fig, dir, x + x_offs, y + y_offs) {
-            self.place_figure(fig, dir, x + x_offs, y + y_offs);
-            return true;
-        }
-        self.place_figure(fig, dir, x, y);
-        return false;
-    }
-}
-
-impl RSTris {
-
-    pub fn new(width: usize, height: usize) -> RSTris {
-        let tris = RSTris{figures: vec![],
-                          pf: RSTrisPlayfield::new(width, height),
-                          current_figure: -1,
-                          fig_dir: 0,
-                          fig_x_pos: 0,
-                          fig_y_pos: 0};
-        tris
-    }
-
-    fn add_figure(&mut self, fig: Figure) {
-        self.figures.push(fig);
-    }
-
-    fn update(&mut self) {
-        if self.current_figure == -1 {
-            self.current_figure =
-                (rand::random::<u8>() % self.figures.len() as u8) as i32;
-            self.fig_dir = 0;
-            self.fig_x_pos = (self.pf.pf_width / 2 - 1) as i32;
-            self.fig_y_pos = 0;
-            let fig = &self.figures[self.current_figure as usize];
-            if self.pf.test_figure(fig, self.fig_dir, self.fig_x_pos,
-                                   self.fig_y_pos) {
-                self.pf.place_figure(fig, self.fig_dir,
-                                     self.fig_x_pos, self.fig_y_pos);
-            }
-            else {
-                // Game over?
-                self.current_figure = -2;
-                println!("Game Over!");
-            }
-        }
-        else if self.current_figure >= 0 {
-            if self.pf.move_figure(&self.figures[self.current_figure as usize],
-                                   self.fig_dir, self.fig_x_pos,
-                                   self.fig_y_pos,
-                                   0, 1) {
-                self.fig_y_pos += 1;
-            }
-            else {
-                // Figure couldn't be moved further - Leave where ut is and
-                // place another figure.
-                self.current_figure = -1;
-            }
-        }
-    }
-
-    fn dump_figure(figure: &Figure) {
-        for d in 0..4 {
-            println!("Direction {}:", d);
-            for y in 0..4 {
-                for x in 0..4 {
-                    print!("{}",
-                           if figure.blocks[d][y][x] == 0 {" " } else {"X"});
-                }
-                println!("");
-            }
-        }
-    }
-
-    fn dump_figures(&self) {
-        for fig in &self.figures {
-            RSTris::dump_figure(fig);
-        }
-
-    }
-}
 
 fn draw_block(renderer: &mut Renderer, x: i32, y: i32, color: Color) {
     let block_width: i32 = 32;
@@ -195,7 +22,7 @@ fn draw_block(renderer: &mut Renderer, x: i32, y: i32, color: Color) {
     let _ = renderer.fill_rect(border_rect);
 }
 
-fn draw_playfield(playfield: &RSTrisPlayfield, renderer: &mut Renderer) {
+fn draw_playfield(playfield: &rstris::Playfield, renderer: &mut Renderer) {
     let frame_color = Color::RGB(200, 64, 64);
     for y in 0..playfield.height() {
         draw_block(renderer, 0, y as i32, frame_color);
@@ -209,140 +36,147 @@ fn draw_playfield(playfield: &RSTrisPlayfield, renderer: &mut Renderer) {
         draw_block(renderer, (playfield.width() + 1) as i32,
                    y as i32, frame_color);
     }
-    for bottom in 0..(playfield.pf_width + 2) {
+    for bottom in 0..(playfield.width() + 2) {
         draw_block(renderer, bottom as i32,
                    playfield.height() as i32, frame_color);
     }
 }
 
-fn init_figures(rstris: &mut RSTris)
+fn init_figures(playfield: &mut rstris::Playfield) //rstris: &mut rstris::RSTris)
 {
-    let fig1 = Figure{blocks: [[[0, 0, 0, 0],
-                                [1, 1, 1, 0],
-                                [0, 1, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[0, 1, 0, 0],
-                                [1, 1, 0, 0],
-                                [0, 1, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[0, 1, 0, 0],
-                                [1, 1, 1, 0],
-                                [0, 0, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[0, 1, 0, 0],
-                                [0, 1, 1, 0],
-                                [0, 1, 0, 0],
-                                [0, 0, 0, 0]]]};
+    let fig1 = rstris::Figure{
+        blocks: [[[0, 0, 0, 0],
+                  [1, 1, 1, 0],
+                  [0, 1, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[0, 1, 0, 0],
+                  [1, 1, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[0, 1, 0, 0],
+                  [1, 1, 1, 0],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[0, 1, 0, 0],
+                  [0, 1, 1, 0],
+                  [0, 1, 0, 0],
+                  [0, 0, 0, 0]]]};
 
-    let fig2 = Figure{blocks: [[[0, 0, 0, 0],
-                                [1, 1, 1, 0],
-                                [0, 0, 1, 0],
-                                [0, 0, 0, 0]],
-                               [[0, 1, 0, 0],
-                                [0, 1, 0, 0],
-                                [1, 1, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[1, 0, 0, 0],
-                                [1, 1, 1, 0],
-                                [0, 0, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[0, 1, 1, 0],
-                                [0, 1, 0, 0],
-                                [0, 1, 0, 0],
-                                [0, 0, 0, 0]]]};
+    let fig2 = rstris::Figure{
+        blocks: [[[0, 0, 0, 0],
+                  [1, 1, 1, 0],
+                  [0, 0, 1, 0],
+                  [0, 0, 0, 0]],
+                 [[0, 1, 0, 0],
+                  [0, 1, 0, 0],
+                  [1, 1, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[1, 0, 0, 0],
+                  [1, 1, 1, 0],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[0, 1, 1, 0],
+                  [0, 1, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 0, 0, 0]]]};
 
-    let fig3 = Figure{blocks: [[[1, 1, 0, 0],
-                                [0, 1, 1, 0],
-                                [0, 0, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[0, 1, 0, 0],
-                                [1, 1, 0, 0],
-                                [1, 0, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[1, 1, 0, 0],
-                                [0, 1, 1, 0],
-                                [0, 0, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[0, 1, 0, 0],
-                                [1, 1, 0, 0],
-                                [1, 0, 0, 0],
-                                [0, 0, 0, 0]]]};
+    let fig3 = rstris::Figure{
+        blocks: [[[1, 1, 0, 0],
+                  [0, 1, 1, 0],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[0, 1, 0, 0],
+                  [1, 1, 0, 0],
+                  [1, 0, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[1, 1, 0, 0],
+                  [0, 1, 1, 0],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[0, 1, 0, 0],
+                  [1, 1, 0, 0],
+                  [1, 0, 0, 0],
+                  [0, 0, 0, 0]]]};
 
-    let fig4 = Figure{blocks: [[[1, 1, 0, 0],
-                                [1, 1, 0, 0],
-                                [0, 0, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[1, 1, 0, 0],
-                                [1, 1, 0, 0],
-                                [0, 0, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[1, 1, 0, 0],
-                                [1, 1, 0, 0],
-                                [0, 0, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[1, 1, 0, 0],
-                                [1, 1, 0, 0],
-                                [0, 0, 0, 0],
-                                [0, 0, 0, 0]]]};
+    let fig4 = rstris::Figure{
+        blocks: [[[1, 1, 0, 0],
+                  [1, 1, 0, 0],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[1, 1, 0, 0],
+                  [1, 1, 0, 0],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[1, 1, 0, 0],
+                  [1, 1, 0, 0],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[1, 1, 0, 0],
+                  [1, 1, 0, 0],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]]]};
 
-    let fig5 = Figure{blocks: [[[0, 1, 1, 0],
-                                [1, 1, 0, 0],
-                                [0, 0, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[1, 0, 0, 0],
-                                [1, 1, 0, 0],
-                                [0, 1, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[0, 1, 1, 0],
-                                [1, 1, 0, 0],
-                                [0, 0, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[1, 0, 0, 0],
-                                [1, 1, 0, 0],
-                                [0, 1, 0, 0],
-                                [0, 0, 0, 0]]]};
+    let fig5 = rstris::Figure{
+        blocks: [[[0, 1, 1, 0],
+                  [1, 1, 0, 0],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[1, 0, 0, 0],
+                  [1, 1, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[0, 1, 1, 0],
+                  [1, 1, 0, 0],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[1, 0, 0, 0],
+                  [1, 1, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 0, 0, 0]]]};
 
-    let fig6 = Figure{blocks: [[[0, 0, 0, 0],
-                                [1, 1, 1, 0],
-                                [1, 0, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[1, 1, 0, 0],
-                                [0, 1, 0, 0],
-                                [0, 1, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[0, 0, 1, 0],
-                                [1, 1, 1, 0],
-                                [0, 0, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[0, 1, 0, 0],
-                                [0, 1, 0, 0],
-                                [0, 1, 1, 0],
-                                [0, 0, 0, 0]]]};
+    let fig6 = rstris::Figure{
+        blocks: [[[0, 0, 0, 0],
+                  [1, 1, 1, 0],
+                  [1, 0, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[1, 1, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[0, 0, 1, 0],
+                  [1, 1, 1, 0],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[0, 1, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 1, 1, 0],
+                  [0, 0, 0, 0]]]};
 
-    let fig7 = Figure{blocks: [[[0, 1, 0, 0],
-                                [0, 1, 0, 0],
-                                [0, 1, 0, 0],
-                                [0, 1, 0, 0]],
-                               [[0, 0, 0, 0],
-                                [1, 1, 1, 1],
-                                [0, 0, 0, 0],
-                                [0, 0, 0, 0]],
-                               [[0, 1, 0, 0],
-                                [0, 1, 0, 0],
-                                [0, 1, 0, 0],
-                                [0, 1, 0, 0]],
-                               [[0, 0, 0, 0],
-                                [1, 1, 1, 1],
-                                [0, 0, 0, 0],
-                                [0, 0, 0, 0]]]};
+    let fig7 = rstris::Figure{
+        blocks: [[[0, 1, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 1, 0, 0]],
+                 [[0, 0, 0, 0],
+                  [1, 1, 1, 1],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]],
+                 [[0, 1, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 1, 0, 0]],
+                 [[0, 0, 0, 0],
+                  [1, 1, 1, 1],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]]]};
 
-    rstris.add_figure(fig1);
-    rstris.add_figure(fig2);
-    rstris.add_figure(fig3);
-    rstris.add_figure(fig4);
-    rstris.add_figure(fig5);
-    rstris.add_figure(fig6);
-    rstris.add_figure(fig7);
+    playfield.add_figure(fig1);
+    playfield.add_figure(fig2);
+    playfield.add_figure(fig3);
+    playfield.add_figure(fig4);
+    playfield.add_figure(fig5);
+    playfield.add_figure(fig6);
+    playfield.add_figure(fig7);
 }
 
 fn main() {
@@ -363,8 +197,10 @@ fn main() {
     renderer.clear();
     renderer.present();
 
-    let mut rstris = RSTris::new(12, 16);
-    init_figures(&mut rstris);
+    let mut pf1 = rstris::Playfield::new(12, 16);
+    init_figures(&mut pf1);
+    let mut rstris = rstris::RSTris::new();
+    rstris.add_playfield(pf1);
 
 
     let mut event_pump = sdl_context.event_pump().unwrap();
@@ -389,7 +225,7 @@ fn main() {
         /* Render graphics */
         let _ = renderer.set_draw_color(Color::RGB(101, 208, 246));
         let _ = renderer.clear();
-        draw_playfield(&rstris.pf, &mut renderer);
+        draw_playfield(&rstris.get_playfields()[0], &mut renderer);
         let _ = renderer.present();
     }
 }
---
+++ src/rstris.rs
@@ -0,0 +1,203 @@
+extern crate rand;
+
+#[derive(Clone, Debug)]
+pub struct Figure {
+    pub blocks: [[[u8; 4]; 4]; 4]
+}
+
+/*
+struct RSTrisLiveFigure {
+    fig: i32,
+    x: i32,
+    y: i32,
+    dir: i32,
+}*/
+
+#[derive(Clone, Debug)]
+struct FigurePosition {
+    fig_index: i32,
+    fig_dir: i32,
+    fig_x_pos: i32,
+    fig_y_pos: i32,
+}
+
+
+#[derive(Debug)]
+pub struct Playfield {
+    pf_width: usize,
+    pf_height: usize,
+    blocks: Vec<Vec<u8>>,
+    figures: Vec<Figure>,
+    figure: FigurePosition,
+}
+
+#[derive(Debug)]
+pub struct RSTris {
+    playfields: Vec<Playfield>,
+    /*
+    current_figure: i32,
+    fig_dir: i32,
+    fig_x_pos: i32,
+    fig_y_pos: i32,
+*/
+}
+
+
+impl Playfield {
+    pub fn new(width: usize, height: usize) -> Playfield {
+        let mut playfield = Playfield{pf_width: width,
+                                      pf_height: height,
+                                      blocks: vec![],
+                                      figures: vec![],
+                                      figure:
+                                      FigurePosition{fig_index: -1,
+                                                     fig_dir: -1,
+                                                     fig_x_pos: 0,
+                                                     fig_y_pos: 0}};
+        for _ in 0..height {
+            playfield.blocks.push(vec![0; width as usize]);
+        }
+        playfield
+    }
+    pub fn add_figure(&mut self, fig: Figure) {
+        self.figures.push(fig);
+    }
+    pub fn get_block(&self, x: usize, y: usize) -> u8 {
+        self.blocks[y][x]
+    }
+    pub fn width(&self) -> usize {
+        self.pf_width
+    }
+    pub fn height(&self) -> usize {
+        self.pf_height
+    }
+
+    fn place_figure(&mut self, fig: &Figure, dir: i32, x: i32, y: i32) {
+        for row in 0..4 {
+            for col in 0..4 {
+                let b = fig.blocks[dir as usize][row][col];
+                if b != 0 {
+                    self.blocks[y as usize + row][x as usize + col] = b;
+                }
+            }
+        }
+    }
+
+    fn rm_figure(&mut self, fig: &Figure, dir: i32, x: i32, y: i32) {
+        for row in 0..4 {
+            for col in 0..4 {
+                let b = fig.blocks[dir as usize][row][col];
+                if b != 0 {
+                    self.blocks[y as usize + row][x as usize + col] = 0;
+                }
+            }
+        }
+    }
+
+    fn test_figure(&self, fig: &Figure, dir: i32, x: i32, y: i32) -> bool {
+        for row in 0..4 {
+            let offs_y  = y + row;
+            for col in 0..4 {
+                let offs_x  = x + col;
+                let b = fig.blocks[dir as usize][row as usize][col as usize];
+                if b != 0 {
+                    if offs_y < 0 || offs_y >= self.pf_height as i32 {
+                        return false;
+                    }
+                    if offs_x < 0 || offs_x >= self.pf_width as i32 {
+                        return false;
+                    }
+                    if self.blocks[offs_y as usize][offs_x as usize] != 0 {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    fn move_figure(&mut self, fig: &Figure, dir: i32, x: i32, y: i32,
+                   x_offs: i32, y_offs: i32) -> bool {
+        self.rm_figure(fig, dir, x, y);
+        if self.test_figure(fig, dir, x + x_offs, y + y_offs) {
+            self.place_figure(fig, dir, x + x_offs, y + y_offs);
+            return true;
+        }
+        self.place_figure(fig, dir, x, y);
+        return false;
+    }
+
+    fn update(&mut self) {
+        let mut figure = self.figure.clone();
+        if figure.fig_index == -1 {
+            figure.fig_index =
+                (rand::random::<u8>() % self.figures.len() as u8) as i32;
+            figure.fig_dir = 0;
+            figure.fig_x_pos = (self.width() / 2 - 1) as i32;
+            figure.fig_y_pos = 0;
+            let fig = self.figures[figure.fig_index as usize].clone();
+            if self.test_figure(&fig, figure.fig_dir,
+                                figure.fig_x_pos,
+                                figure.fig_y_pos) {
+                self.place_figure(&fig, figure.fig_dir,
+                                  figure.fig_x_pos,
+                                  figure.fig_y_pos);
+            }
+            else {
+                // Game over?
+                figure.fig_index = -2;
+                println!("Game Over!");
+            }
+        }
+        else if figure.fig_index >= 0 {
+            let fig = self.figures[figure.fig_index as usize].clone();
+            if self.move_figure(&fig,
+                                figure.fig_dir,
+                                figure.fig_x_pos,
+                                figure.fig_y_pos,
+                                0, 1) {
+                figure.fig_y_pos += 1;
+            }
+            else {
+                // Figure couldn't be moved further - Leave where ut is and
+                // place another figure.
+                figure.fig_index = -1;
+            }
+        }
+        self.figure = figure;
+    }
+}
+
+impl RSTris {
+
+    pub fn new() -> RSTris {
+        RSTris{playfields: vec![]}
+    }
+
+    pub fn add_playfield(&mut self, playfield: Playfield) {
+        self.playfields.push(playfield);
+    }
+
+    pub fn get_playfields(&self) -> &Vec<Playfield> {
+        &self.playfields
+    }
+
+    pub fn update(&mut self) {
+        for i in 0..self.playfields.len() {
+            self.playfields.get_mut(i).unwrap().update();
+        }
+    }
+
+    fn dump_figure(figure: &Figure) {
+        for d in 0..4 {
+            println!("Direction {}:", d);
+            for y in 0..4 {
+                for x in 0..4 {
+                    print!("{}",
+                           if figure.blocks[d][y][x] == 0 {" " } else {"X"});
+                }
+                println!("");
+            }
+        }
+    }
+}
