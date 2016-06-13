83eb937c631db2cc4cb04480095ec60f5b42d818
Olle Sandberg <olle@b3rg.se>
2016-06-13 08:25:58 +0200

    Remove mod.rs, not used.

--- rstris/src/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod position;
-pub mod playfield;
-pub mod figure;
-pub mod player;
-
-pub use rstris::position::*;
-pub use rstris::figure::*;
-pub use rstris::playfield::*;
-pub use rstris::player::*;
