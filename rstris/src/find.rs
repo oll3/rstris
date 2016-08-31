72b48de02b447fcb8e4613878e9b1232f2a61118
Olle Sandberg <olle@b3rg.se>
2016-08-31 13:09:26 +0200

    Test for locked blocks when searching for valid placing.

--- rstris/src/find.rs
+++ rstris/src/find.rs
@@ -24,7 +24,7 @@ pub fn get_valid_placing(pf: &Playfield,
 
     println!("Find valid placements for figure {} (starting at {:?})",
              fig.get_name(), start_pos);
-    if fig.collide_blocked(pf, &start_pos) {
+    if fig.collide_locked(pf, &start_pos) {
         println!("Invalid starting point ({:?}) for figure {}",
                  start_pos, fig.get_name());
         return placements;
@@ -40,13 +40,13 @@ pub fn get_valid_placing(pf: &Playfield,
         // already (one left, right, down, rotate cw).
         let tmp_pos = Position::apply_move(&current_pos,
                                            &Movement::MoveLeft);
-        if !visited.contains(&tmp_pos) && !fig.collide_blocked(&pf, &tmp_pos) {
+        if !visited.contains(&tmp_pos) && !fig.collide_locked(&pf, &tmp_pos) {
             visited.insert(tmp_pos.clone());
             moves.push_back(tmp_pos);
         }
         let tmp_pos = Position::apply_move(&current_pos,
                                            &Movement::MoveRight);
-        if !visited.contains(&tmp_pos) && !fig.collide_blocked(&pf, &tmp_pos) {
+        if !visited.contains(&tmp_pos) && !fig.collide_locked(&pf, &tmp_pos) {
             visited.insert(tmp_pos.clone());
             moves.push_back(tmp_pos);
         }
@@ -54,7 +54,7 @@ pub fn get_valid_placing(pf: &Playfield,
                                            &Movement::RotateCW);
         if tmp_pos.get_dir() < fig.get_num_dirs() as i32 &&
             !visited.contains(&tmp_pos) &&
-            !fig.collide_blocked(&pf, &tmp_pos) {
+            !fig.collide_locked(&pf, &tmp_pos) {
                 visited.insert(tmp_pos.clone());
                 moves.push_back(tmp_pos);
         }
@@ -63,7 +63,7 @@ pub fn get_valid_placing(pf: &Playfield,
         // the current position is a valid placement.
         let tmp_pos = Position::apply_move(&current_pos,
                                            &Movement::MoveDown);
-        if fig.collide_blocked(&pf, &tmp_pos) {
+        if fig.collide_locked(&pf, &tmp_pos) {
             // Valid placement
             println!("Valid position: {:?}", tmp_pos);
             placements.push(current_pos.clone());
