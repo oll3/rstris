d0442d183f80b01f44ae2f0cb7f90ba5bfc8cb50
Olle Sandberg <olle@b3rg.se>
2016-10-11 13:26:51 +0200

    matrix_2d.rs: Add new_init() which initialize the matrix from a slice of slices.

--- rstris/src/matrix_2d.rs
+++ rstris/src/matrix_2d.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone, RustcDecodable, RustcEncodable)]
+#[derive(Debug, Clone, Hash, Eq, PartialEq, RustcDecodable, RustcEncodable)]
 pub struct Matrix2D<T> {
     width: usize,
     height: usize,
@@ -6,13 +6,27 @@ pub struct Matrix2D<T> {
 }
 
 impl<T> Matrix2D<T> where T: Clone {
-    pub fn new(width: usize, height: usize, initial_value: T)
-               -> Self  {
+    pub fn new(width: usize, height: usize, initial_value: T) -> Self  {
         Matrix2D {
             width: width,
             height: height,
             items: vec![initial_value; width * height],
         }
+    }
+    pub fn new_init(items: &[&[T]]) -> Self {
+        let height = items.len();
+        let width = if height == 0 { 0 } else { items[0].len() };
+        let mut m = Matrix2D {
+            width: width,
+            height: height,
+            items: vec![items[0][0].clone(); width * height],
+        };
+        for y in 0..height {
+            for x in 0..width {
+                m.set(x, y, items[y][x].clone());
+            }
+        }
+        return m;
     }
     pub fn get(&self, x: usize, y: usize) -> &T {
         &self.items[y * self.width + x]
